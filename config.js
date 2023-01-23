c2fc4498e853874c74e5d85f68286c188663b3f6
Tohrou <nwgwdarzer@gmail.com>
2023-01-22 22:07:32 -0500

    initial

---
+++ config.js
@@ -0,0 +1,230 @@
+// ╔╗ ╔═╗╔╗╔╔╦╗╔═╗
+// ╠╩╗║╣ ║║║ ║ ║ ║
+// ╚═╝╚═╝╝╚╝ ╩ ╚═╝
+// ┌─┐┌─┐┌┐┌┌─┐┬┌─┐┬ ┬┬─┐┌─┐┌┬┐┬┌─┐┌┐┌
+// │  │ ││││├┤ ││ ┬│ │├┬┘├─┤ │ ││ ││││
+// └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
+
+const CONFIG = {
+	// ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+	// ├┴┐├─┤└─┐││  └─┐
+	// └─┘┴ ┴└─┘┴└─┘└─┘
+
+	// General
+	name: 'TOHROU',
+	imageBackground: true,
+	openInNewTab: true,
+	twelveHourFormat: true,
+
+	// Greetings
+	greetingMorning: 'Good morning!',
+	greetingAfternoon: 'Good afternoon,',
+	greetingEvening: 'Good evening,',
+	greetingNight: 'Go to Sleep!',
+
+	// Layout
+	bentoLayout: 'lists', // 'bento', 'lists', 'buttons'
+
+	// Weather
+	weatherKey: '366e0799822db547ce1389ff6703a7e8', // Write here your API Key
+	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherUnit: 'F', // 'F', 'C'
+	language: 'en', // More languages in https://openweathermap.org/current#multi
+
+	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
+	defaultLatitude: '36.595104',
+	defaultLongitude: '-82.188744',
+
+	// Autochange
+	autoChangeTheme: false,
+
+	// Autochange by OS
+	changeThemeByOS: false,
+
+	// Autochange by hour options (24hrs format, string must be in: hh:mm)
+	changeThemeByHour: false,
+	hourDarkThemeActive: '18:30',
+	hourDarkThemeInactive: '07:00',
+
+	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+	// ├┴┐│ │ │  │ │ ││││└─┐
+	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+	firstButtonsContainer: [
+		{
+			id: '1',
+			name: 'Github',
+			icon: 'github',
+			link: 'https://github.com/',
+		},
+		{
+			id: '2',
+			name: 'Mail',
+			icon: 'mail',
+			link: 'https://mail.protonmail.com/',
+		},
+		{
+			id: '3',
+			name: 'Todoist',
+			icon: 'trello',
+			link: 'https://todoist.com',
+		},
+		{
+			id: '4',
+			name: 'Calendar',
+			icon: 'calendar',
+			link: 'https://calendar.google.com/calendar/r',
+		},
+		{
+			id: '5',
+			name: 'Reddit',
+			icon: 'glasses',
+			link: 'https://reddit.com',
+		},
+		{
+			id: '6',
+			name: 'Odysee',
+			icon: 'youtube',
+			link: 'https://odysee.com/',
+		},
+	],
+
+	secondButtonsContainer: [
+		{
+			id: '1',
+			name: 'Music',
+			icon: 'headphones',
+			link: 'https://open.spotify.com',
+		},
+		{
+			id: '2',
+			name: 'twitter',
+			icon: 'twitter',
+			link: 'https://twitter.com/',
+		},
+		{
+			id: '3',
+			name: 'bot',
+			icon: 'bot',
+			link: 'https://discord.com/app',
+		},
+		{
+			id: '4',
+			name: 'Amazon',
+			icon: 'shopping-bag',
+			link: 'https://amazon.com/',
+		},
+		{
+			id: '5',
+			name: 'Hashnode',
+			icon: 'pen-tool',
+			link: 'https://hashnode.com/',
+		},
+		{
+			id: '6',
+			name: 'Figma',
+			icon: 'figma',
+			link: 'https://figma.com/',
+		},
+	],
+
+	// ┬  ┬┌─┐┌┬┐┌─┐
+	// │  │└─┐ │ └─┐
+	// ┴─┘┴└─┘ ┴ └─┘
+
+	// First Links Container
+	firstlistsContainer: [
+		{
+			icon: 'play-circle',
+			id: '1',
+			links: [
+				{
+					name: 'YouTube',
+					link: 'https://www.youtube.com/',
+				},
+				{
+					name: 'YouTube Music',
+					link: 'https://music.youtube.com/',
+				},
+				{
+					name: '9Anime',
+					link: 'https://www.9anime.to/',
+				},
+				{
+					name: 'MyAnimeList',
+					link: 'https://myanimelist.net/',
+				},
+			],
+		},
+		{
+			icon: 'twitter',
+			id: '2',
+			links: [
+				{
+					name: 'Reddit',
+					link: 'https://www.reddit.com/',
+				},
+				{
+					name: 'Twitter',
+					link: 'https://www.twitter.com/',
+				},
+				{
+					name: 'e621',
+					link: 'https://www.e621.net/',
+				},
+				{
+					name: '',
+					link: '',
+				},
+			],
+		},
+	],
+
+	// Second Links Container
+	secondListsContainer: [
+		{
+			icon: 'gamepad',
+			id: '1',
+			links: [
+				{
+					name: 'Roblox',
+					link: 'https://www.roblox.com/',
+				},
+				{
+					name: 'Underground Community',
+					link: 'https://cs.rin.ru/',
+				},
+				{
+					name: 'Stardew Wiki',
+					link: 'https://stardewvalleywiki.com/Stardew_Valley_Wiki',
+				},
+				{
+					name: 'Stardew Expanded Wiki',
+					link: 'https://stardew-valley-expanded.fandom.com/wiki/Stardew_Valley_Expanded_Wiki',
+				},
+			],
+		},
+		{
+			icon: 'mail',
+			id: '2',
+			links: [
+				{
+					name: 'Gmail',
+					link: 'https://mail.google.com/mail/',
+				},
+				{
+					name: 'PayPal',
+					link: 'https://www.paypal.com/',
+				},
+				{
+					name: 'Amazon',
+					link: 'https://www.amazon.com/',
+				},
+				{
+					name: 'Yahoo',
+					link: 'https://www.yahoo.com/',
+				},
+			],
+		},
+	],
+};
